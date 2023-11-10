e70d0b8a59c9495d19f2a47e9812cd8c4ef7ddf8
Darrell <darrell.anderson@gmail.com>
2023-11-10 10:41:10 -0800

    refactor card generator (currently assumes shared back)

--- tooling/generators/create-deck.ts
+++ tooling/generators/create-deck.ts
@@ -40,11 +40,12 @@
  * % ls -1 *\*.jpg |sed -e "s/.jpg$//" | awk -F\/ '{print "{ @face@: @" $1"/"$2".jpg@,@name@: @"toupper(substr($1, 1, 1)) substr($1, 2)" "$2"@,@metadata@: @card.action."$1":base/"$2"@}," }' | tr '@' '"'
  */
 
-import * as sharp from "sharp";
 import * as fs from "fs-extra";
 import * as yargs from "yargs";
 import * as path from "path";
 import * as crypto from "crypto";
+
+import { MergeImage, MergeImageResult } from "./lib/merge-image";
 
 const args = yargs
     .options({
@@ -163,193 +164,47 @@ async function main() {
     config.inputCards = origInputCards;
 
     // ------------------------------------
-    console.log("\n----- VERIFY AND PRESHRINK INPUT CARDS -----\n");
-
-    const origPixels = { w: -1, h: -1 }; // seed with first card loaded
-    const cardPixels = { w: -1, h: -1 }; // seed with first card loaded APPLY PRESHINK
-    for (const inputCard of config.inputCards) {
-        for (const key of ["face", "back"]) {
-            const localFilename = inputCard[key];
-            if (!localFilename) {
-                continue;
-            }
-            if (typeof localFilename !== "string") {
-                throw new Error("inputCards filename not a string");
-            }
-
-            // Compute input file name, store with card data.
-            const file = path.join(
-                DIR_INPUT_PREBUILD,
-                config.inputDir,
-                path.normalize(localFilename)
-            );
-
-            // File exists.
-            if (!fs.existsSync(file)) {
-                throw new Error(`Missing inputCard "${file}"`);
-            }
-
-            // Load file, store with card data.
-            const img = sharp(file);
-
-            // All cards same size.
-            const stats = await img.metadata();
-            if (!stats.width || !stats.height) {
-                throw new Error(
-                    `sharp metadata missing width or height for "${file}"`
-                );
-            }
-            if (origPixels.w === -1) {
-                origPixels.w = stats.width;
-                origPixels.h = stats.height;
-            } else if (
-                origPixels.w !== stats.width ||
-                origPixels.h !== stats.height
-            ) {
-                throw new Error(
-                    `card size mismatch (expect ${origPixels.w}x${origPixels.h}, got ${stats.width}x${stats.height}) for ${file}`
-                );
-            }
-
-            // Compute preshrink dimensions.
-            if (cardPixels.w === -1) {
-                const scaleW = Math.min(config.preshrink / stats.width, 1);
-                const scaleH = Math.min(config.preshrink / stats.height, 1);
-                const scale =
-                    config.preshrink > 0 ? Math.min(scaleW, scaleH) : 1;
-                const w = Math.floor(stats.width * scale);
-                const h = Math.floor(stats.height * scale);
-                cardPixels.w = w;
-                cardPixels.h = h;
-            }
-
-            // ALWAYS resize card, always deal with the same img type later.
-            console.log(`processing ${file} (${cardPixels.w}x${cardPixels.h})`);
-            inputCard[key] = await img
-                .resize(cardPixels.w, cardPixels.h, {
-                    fit: "fill",
-                })
-                .toBuffer();
-        }
-    }
-    if (cardPixels.w < 0) {
-        throw new Error("no cards?");
-    }
-
-    // ------------------------------------
-    console.log("\n----- CALCULATING OPTIMAL LAYOUT -----\n");
-
-    const totalCardPixels =
-        config.inputCards.length * cardPixels.w * cardPixels.h;
-
-    const layout = {
-        cols: -1,
-        rows: -1,
-        efficiency: -1,
-    };
-    const maxCols = Math.floor(CHUNK_SIZE / cardPixels.w);
-    const maxRows = Math.floor(CHUNK_SIZE / cardPixels.h);
-    const maxCards = maxCols * maxRows;
-    if (config.inputCards.length > maxCards) {
-        throw new Error(
-            `too many cards (${config.inputCards.length}), max ${maxCards}`
-        );
-    }
-    for (let numCols = 1; numCols <= maxCols; numCols += 1) {
-        const numRows = Math.ceil(config.inputCards.length / numCols);
-        if (numRows > maxRows) {
-            continue; // too big, skip
-        }
-        if (numCols > config.inputCards.length) {
-            continue; // too few cards, skip
-        }
-        const w = numCols * cardPixels.w;
-        const h = numRows * cardPixels.h;
-        const pow2w = Math.pow(2, Math.ceil(Math.log2(w)));
-        const pow2h = Math.pow(2, Math.ceil(Math.log2(h)));
-        const totalLayoutPixels = pow2w * pow2h;
-        let efficiency = (totalCardPixels * 100) / totalLayoutPixels;
-
-        // Favor square layouts when breaking ties.
-        const tiebreaker = Math.abs(w - h) / 100000;
-        efficiency -= tiebreaker;
-
-        if (efficiency > layout.efficiency) {
-            layout.cols = numCols;
-            layout.rows = numRows;
-            layout.efficiency = efficiency;
-        }
-        console.log(
-            `candidate ${numCols}x${numRows} [${w}x${h}] => [${pow2w}x${pow2h}px] (${efficiency.toFixed(
-                5
-            )}%)`
-        );
-    }
-    console.log(
-        `layout ${layout.cols}x${layout.rows} (${layout.efficiency.toFixed(
-            1
-        )}%)`
-    );
-
-    // ------------------------------------
     console.log("\n----- CREATING FACE CARDSHEET -----\n");
+    const cardsheetFaceRelativeToAssetsTextures = config.output + ".face.jpg";
 
-    const cardsheetFaceRelativeToAssetsTextures =
-        path.normalize(config.output) + ".face.jpg";
-    let dstFile = path.join(
-        DIR_OUTPUT_TEXTURE,
-        cardsheetFaceRelativeToAssetsTextures
-    );
-
-    const composite: object[] = [];
-    for (let index = 0; index < config.inputCards.length; index++) {
-        const inputCard = config.inputCards[index];
-        const col = index % layout.cols;
-        const row = Math.floor(index / layout.cols);
-        composite.push({
-            input: inputCard.face,
-            top: row * cardPixels.h,
-            left: col * cardPixels.w,
-        });
-    }
-
-    console.log(`writing "${dstFile}"`);
-    let dir = path.dirname(dstFile);
-    fs.mkdirsSync(dir);
-    await sharp({
-        create: {
-            width: layout.cols * cardPixels.w,
-            height: layout.rows * cardPixels.h,
-            channels: 4,
-            background: { r: 0, g: 0, b: 0, alpha: 1 },
-        },
-    })
-        .composite(composite)
-        .toFile(dstFile);
+    const mergeImageResult: MergeImageResult = await new MergeImage()
+        .setSrcFilesRelativeToPrebuild(
+            config.inputCards.map((x: any) =>
+                path.join(
+                    path.normalize(config.inputDir),
+                    path.normalize(x.face)
+                )
+            )
+        )
+        .setDstFileRelativeToAssetsTextures(
+            cardsheetFaceRelativeToAssetsTextures
+        )
+        .setPreShrink(config.preshrink)
+        .merge();
+    const layout = {
+        cols: mergeImageResult.cols,
+        rows: mergeImageResult.rows,
+    };
 
     // ------------------------------------
     console.log("\n----- CREATING BACK CARDSHEET -----\n");
 
-    const srcFile = path.join(DIR_INPUT_PREBUILD, config.sharedBack);
-    if (!fs.existsSync(srcFile)) {
-        throw new Error(`missing sharedBack "${srcFile}"`);
-    }
     const cardsheetBackRelativeToAssetsTextures =
         path.normalize(config.output) + ".back.jpg";
-    dstFile = path.join(
-        DIR_OUTPUT_TEXTURE,
-        cardsheetBackRelativeToAssetsTextures
-    );
 
-    console.log(`writing "${dstFile}"`);
-    dir = path.dirname(dstFile);
-    fs.mkdirsSync(dir);
-    fs.copyFileSync(srcFile, dstFile);
+    // Boring merge of a single image (resizes).
+    await new MergeImage()
+        .setSrcFilesRelativeToPrebuild([config.sharedBack])
+        .setDstFileRelativeToAssetsTextures(
+            cardsheetBackRelativeToAssetsTextures
+        )
+        .setPreShrink(config.preshrink)
+        .merge();
 
     // ------------------------------------
     console.log("\n----- CREATING DECK TEMPLATE -----\n");
 
-    dstFile = path.join(
+    const dstFile = path.join(
         DIR_OUTPUT_TEMPLATE,
         path.normalize(config.output) + ".json"
     );
@@ -385,7 +240,7 @@ async function main() {
     }
 
     console.log(`writing "${dstFile}"`);
-    dir = path.dirname(dstFile);
+    const dir = path.dirname(dstFile);
     fs.mkdirsSync(dir);
     const data: string = JSON.stringify(template, null, 8);
     fs.writeFileSync(dstFile, data);
