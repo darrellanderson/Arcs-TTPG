6881af9492c29ada465572c1b51619c8d1bc9d6d
Darrell <darrell.anderson@gmail.com>
2023-11-18 09:12:42 -0800

    demo for wacky stuff

---
+++ src/usage.test.ts
@@ -0,0 +1,10 @@
+const { UIZoomVisibility, Vector } = require("@tabletop-playground/api");
+//import { UIZoomVisibility, Vector } from "@tabletop-playground/api";
+const { MockVector } = require("./mock/mock-vector");
+
+it("usage", () => {
+    //new Vector(0, 0, 0);
+    //new MockVector(0, 0, 0);
+    //const a = UIZoomVisibility.Regular;
+    //const b = 0;
+});
